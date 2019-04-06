f2bf6f99203c51688b4f9b7dae68cf48e42d663f
Vikaas1221 <49087740+Vikaas1221@users.noreply.github.com>
2019-04-06 17:58:33 +0530

    Add files via upload

--- roundrobin.c
+++ roundrobin.c
@@ -1,25 +1,29 @@
 #include<stdio.h> 
 void round_robin(int n)
 {
-	int i,j,time,remain_process,time_quantum,m; 
-	remain_process=n;
-  int wait_time=0,turnaround_time=0,arrive_t[10],burst_t[10],rem_t[10]; 
+	int i,j,time,remain_process,time_quantum;
+	int m,t1,xm=0,sm=1;
+    int wait_time=0,turnaround_time=0;
+    int arrive_t[10],burst_t[10],rem_t[10],priority_t[10],ptr[10];
+    remain_process=n; 
   
   
   for(i=0;i<n;i++) 
   { 
-    printf("Enter Arrival Time for process %d :",i+1); 
+    printf("Enter Arrival Time for process%d :",i+1); 
     scanf("%d",&arrive_t[i]); 
-    printf("Enter Burst Time for process %d :",i+1);
+    printf("Enter Burst Time for process%d :",i+1);
     scanf("%d",&burst_t[i]);
-	
     rem_t[i]=burst_t[i]; 
+    printf("Enter Priority for process%d",i+1);
+	scanf("%d",&priority_t[i]);
+    
   }
-  printf("Process   ArrivalTime   BurstTime  \n");
+  printf("Process   ArrivalTime   BurstTime   Priority \n");
   for(i=0;i<n;i++)
   {
   	
-	printf("P[%d]   %d            %d           \n",i+1,arrive_t[i],burst_t[i]);
+	printf("P[%d]   %d                %d               %d\n",i+1,arrive_t[i],burst_t[i],priority_t[i]);
   }
   printf("Enter Time Quantum:\t"); 
   scanf("%d",&time_quantum); 
@@ -52,8 +56,40 @@ void round_robin(int n)
     else 
       i=0; 
   } 
-  printf("\nAverage Waiting Time= %f\n",wait_time*1.0/n); 
+  printf("\nAvg Waiting Time= %f\n",wait_time*1.0/n); 
   printf("Avg Turnaround Time = %f",turnaround_time*1.0/n);
+  for(i=0;i<n;i++)
+  {
+  	ptr[i]=priority_t[i];
+  }
+  for (i=0;i <n;i++)
+    {
+        for (j =0; j<(n-i-1);j++)
+        {
+            if (ptr[j] >ptr[j + 1])
+            {
+                t1 =ptr[j];
+                ptr[j] = ptr[j + 1];
+                ptr[j + 1] = t1;
+            }
+        }
+    }
+  for(i=0;i<n;i++)
+  {
+  	for(j=0;j<n;j++)
+  	{
+  		if((ptr[i]==priority_t[j])&&(ptr[i]!=0))
+  		{
+		xm=j+1;
+  		printf("\nP[%d]->%d",xm,sm);
+  		sm=sm+1;
+	  }
+	  if(ptr[i]==0)
+	  {
+	  	printf("\nP[%d]->%d",sm);
+	  }
+}
+  }
 }
 int main() 
 { 
